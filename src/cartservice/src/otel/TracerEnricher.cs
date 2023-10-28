4d5fe6392cd5b5466a40a49ad8caf0a5b6334c97
Daniel Moll <dmoll1974@gmail.com>
2023-10-28 16:55:48 +0200

    removed Geneva

--- src/cartservice/src/otel/TracerEnricher.cs
+++ src/cartservice/src/otel/TracerEnricher.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OpenTelemetry;
-using OpenTelemetry.Exporter.Geneva;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
